5e7a34747dc7017cfd236d579899b01a1925d60e
Roberto Russo <robertorusso@mail.com>
2024-05-28 16:47:44 +0200

    delete

--- lib/application/shopdata/shopdata_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'shopdata_bloc.dart';
-
-@freezed
-abstract class ShopdataEvent with _$ShopdataEvent {
-  const factory ShopdataEvent.watchAllProducts() = _Started;
-}
--- lib/domain/i_shopdata_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:assignment/infrastructure/shopdata/data_entity.dart';
-
-abstract class IshopDataRepository {
-  Future<List<Datum>> watchAllProducts();
-}
